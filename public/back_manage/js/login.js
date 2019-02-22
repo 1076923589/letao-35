10b5b7650472366fe96a07c1ed7dba082e8b703f
zhangliang <1076923589@qq.com>
2019-02-22 12:06:23 +0800

    基本登录功能ajax提交完成

--- public/back_manage/js/login.js
+++ public/back_manage/js/login.js
@@ -45,7 +45,43 @@ $('#form').bootstrapValidator( {
                 }
             }
         }
+    }  
+ })
+
+// 使用submit 按鈕,會進行表單提交,此時表單效驗插件會立刻進行效驗
+// 1.效驗成功,此時會默認提交,發生頁面跳轉註冊表單效驗成功實踐,在事件中阻止表單的默認提交
+// 2.效驗失敗,自動攔截提交
+
+// 註冊表單效驗成功實踐,在事件中阻止默認的提交,通過ajax提交
+$('#form').on('success.form.bv', function( e ) {
+e.preventDefault();  // 阻止默認事件
+
+// console.log('當前事件被阻止');
+
+$.ajax({
+    url: '/employee/employeeLogin',
+    type:'post',
+    // 表單序列化,自動將所有配置了name屬性的input值進行普拼接 用於提交
+
+    data: $('#form').serialize(),
+    dataType: 'json',
+//     success:function(){
+    success: function(info) {
+        console.log(info);
+        if (info.error === 1000) {
+            alert('用戶名不存在')
+        }
+        if (info.error === 1001) {
+            alert('密碼輸入有誤')
+        }
+        if (info.success) {
+            // 登錄成功,需要跳轉
+            location.href = 'index.html'
+        }
     }
+//     }
+
+})
 
 })
 
