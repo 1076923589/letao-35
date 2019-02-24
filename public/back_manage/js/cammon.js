6761460ae6d192d2739ca1584bb9d7fd3d36624f
zhangliang <1076923589@qq.com>
2019-02-24 21:39:57 +0800

    公共部分基本完成2.24

--- public/back_manage/js/cammon.js
+++ public/back_manage/js/cammon.js
@@ -1,10 +1,10 @@
 // 进度条功能
 // 开始进度条
-// NProgress.start();
+NProgress.start();
 
-// setTimeout(function(){
-//     NProgress.done();
-// }, 2000);
+setTimeout(function(){
+    NProgress.done();
+}, 500);
 
 
 
@@ -32,4 +32,58 @@ $(document).ajaxStart(function(){
     setTimeout(function() {
         NProgress.done();
     },500);
+});
+
+
+// 公共部分功能
+// 1.左侧二级菜单的切换
+// 2.左侧整体彩带的切换
+// 3.公共退出功能
+
+// 等待dom加完完成执行
+
+$(function () {
+    // 左侧二级菜单切换
+    $('.lt_aside .category').click(function(){
+    // 找到下一个兄弟元素,显示
+    $(this).next().stop().slideToggle();
+//    console.log($(this));
+    // console.log(this);
+    });
+
+//    console.log($('.lt_topbar .icon_menu'));
+//    2.左侧整体菜单的切换
+$('.lt_topbar .icon_menu').click(function(){
+    console.log(1234);
+    // 让左侧菜单切换显示
+    $('.lt_aside').toggleClass('hidemenu');
+    $('.lt_main').toggleClass('hidemenu');
+    $('.lt_topbar').toggleClass('hidemenu');
+
+});
+
+// 3.退出功能
+// 点击退出按钮,显示一个模态框
+$('.lt_topbar .icon_logout').click(function(){
+    $('#logoutModal').modal('show')
+});
+
+// 点击模态框的退出按钮,表示确认退出
+$('#logoutBtn').click(function() {
+    // location = 'login.html'
+    $.ajax({
+        type: 'get',
+        url: '/employee/employeeLogout',
+        dataType: 'json',
+        success: function( info ){
+            if (info.success) {
+                // 退出成功
+                location = 'login.html'
+
+            }
+            
+        }
+
+    })
+})
 })
